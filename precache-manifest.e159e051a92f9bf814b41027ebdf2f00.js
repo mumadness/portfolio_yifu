370c5b5ab9a7984eb5f0219ede6ce48fb03f8cf8
mumadness <yifusworld@gmail.com>
2020-09-30 20:18:10 -0400

    Updates

---
+++ precache-manifest.e159e051a92f9bf814b41027ebdf2f00.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b3f36e7c73bff042e0cd2886c32a84",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45b43e778bf099004eb8",
+    "url": "/portfolio/static/css/main.298cf8e8.chunk.css"
+  },
+  {
+    "revision": "be6b77e778811b5202f0",
+    "url": "/portfolio/static/js/2.46091dda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.46091dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b43e778bf099004eb8",
+    "url": "/portfolio/static/js/main.a44f4178.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "30b689b616ac88311724c962a77f018d",
+    "url": "/portfolio/static/media/american-idiot.30b689b6.jpg"
+  },
+  {
+    "revision": "8450cf552bd8b57785c958863dd9c6f3",
+    "url": "/portfolio/static/media/cambodia.8450cf55.jpg"
+  },
+  {
+    "revision": "3cc53f2aeac4a32f8b63035efec83961",
+    "url": "/portfolio/static/media/capitol-gs.3cc53f2a.jpg"
+  },
+  {
+    "revision": "d8a083f75c80c2d6b667ae7b99063b3a",
+    "url": "/portfolio/static/media/montana-sun.d8a083f7.jpg"
+  },
+  {
+    "revision": "561b160b8adebf6bc7c0eca0c262ecb7",
+    "url": "/portfolio/static/media/taormina-etna.561b160b.jpg"
+  },
+  {
+    "revision": "d43dee73ea17ad27f0430b975923a095",
+    "url": "/portfolio/static/media/tufts-steps.d43dee73.jpg"
+  },
+  {
+    "revision": "bd2aaff64b3d868b5cb28becb5880463",
+    "url": "/portfolio/static/media/varsity-bball.bd2aaff6.jpg"
+  }
+]);
